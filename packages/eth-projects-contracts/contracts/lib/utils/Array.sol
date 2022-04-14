38c35b4456b0056152f2a1255def4e26aa92bf44
Clement Walter <clement.walter@ibm.com>
2022-04-14 10:03:55 +0200

    Add Array.join for value types

--- packages/eth-projects-contracts/contracts/lib/utils/Array.sol
+++ packages/eth-projects-contracts/contracts/lib/utils/Array.sol
@@ -8,6 +8,8 @@
  */
 pragma solidity ^0.8.12;
 
+error EmptyArray();
+
 library Array {
     function join(string[] memory a) public pure returns (string memory) {
         bytes memory tempBytes;
@@ -62,5 +64,122 @@ library Array {
             mstore(0x40, and(add(memoryPointer, 31), not(31)))
         }
         return string(tempBytes);
+    }
+
+    function join(bytes1[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes2[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes3[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes4[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes8[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes16[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function join(bytes32[] memory a) public pure returns (bytes memory) {
+        uint256 inputLength = a.length;
+        if (inputLength == 0) revert EmptyArray();
+        uint256 typeLength = a[0].length;
+
+        uint256 inputData;
+        assembly {
+            inputData := add(a, 0x20)
+        }
+        return _joinValueType(typeLength, inputLength, inputData);
+    }
+
+    function _joinValueType(
+        uint256 typeLength,
+        uint256 inputLength,
+        uint256 inputData
+    ) private pure returns (bytes memory) {
+        bytes memory tempBytes;
+
+        assembly {
+            let end := add(inputData, mul(inputLength, 0x20))
+
+            // Get a location of some free memory and store it in tempBytes as
+            // Solidity does for memory variables.
+            tempBytes := mload(0x40)
+
+            // Initialize the length of the final bytes: length is typeLength x inputLength (array of bytes4)
+            mstore(tempBytes, mul(inputLength, typeLength))
+            let memoryPointer := add(tempBytes, 0x20)
+
+            // Iterate over all bytes4
+            for {
+                let pointer := inputData
+            } lt(pointer, end) {
+                pointer := add(pointer, 0x20)
+            } {
+                mstore(memoryPointer, mload(pointer))
+                memoryPointer := add(memoryPointer, typeLength)
+            }
+
+            mstore(0x40, and(add(memoryPointer, 31), not(31)))
+        }
+        return tempBytes;
     }
 }
--- packages/eth-projects-contracts/test/contracts/Array.test.ts
+++ packages/eth-projects-contracts/test/contracts/Array.test.ts
@@ -21,19 +21,47 @@ const setup = async () => {
 
 describe("Array", function () {
   describe("join", async function () {
-    [...Array(100).keys()].forEach((stringCount) => {
-      [...Array(100).keys()].forEach((stringLength) => {
-        it(`should return the correct string for ${
-          stringCount + 1
-        } strings of length ${stringLength + 1}`, async function () {
-          const { ArraySol } = await setup();
-          const strings = [...Array(stringCount + 1).keys()].map(
-            (j) => `${j}${"-".repeat(stringLength)}`
-          );
-          const result = await ArraySol.join(strings);
-          expect(result).to.equal(strings.join(""));
+    describe("join(string[])", async function () {
+      [...Array(100).keys()].forEach((stringCount) => {
+        [...Array(100).keys()].forEach((stringLength) => {
+          it(`should return the correct string for ${
+            stringCount + 1
+          } strings of length ${stringLength + 1}`, async function () {
+            const { ArraySol } = await setup();
+            const strings = [...Array(stringCount + 1).keys()].map(
+              (j) => `${j}${"-".repeat(stringLength)}`
+            );
+            const result = await ArraySol["join(string[])"](strings);
+            expect(result).to.equal(strings.join(""));
+          });
         });
       });
     });
+
+    [1, 2, 3, 4, 8, 16, 32]
+      .map((l) => ({
+        length: l,
+        key: `join(bytes${l}[])`,
+      }))
+      .forEach((type) => {
+        describe.only(`bytes${type.length}[]`, function () {
+          [...Array(100).keys()].forEach((length) => {
+            it(`should return the correct bytes for bytes${type.length}[${
+              length + 1
+            }]`, async function () {
+              const { ArraySol } = await setup();
+              const inputArray = [...Array(length + 1).keys()].map((j) =>
+                j.toString(16).padEnd(type.length * 2, "0")
+              );
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              const result = await ArraySol[type.key](
+                inputArray.map((b) => "0x" + b)
+              );
+              expect(result).to.equal("0x" + inputArray.join(""));
+            });
+          });
+        });
+      });
   });
 });
